dc67f8014957e305c668ac08b128a4a7013622a2
cbx <cbx@LAPTOP-VCAB9RNU.lan>
2016-11-23 22:58:09 +0800

    内存泄漏例子

---
+++ src/main/java/com/cn21/test/javaVMStackSOF.java
@@ -0,0 +1,31 @@
+package com.cn21.test;
+
+/**
+ * VM Args: -Xss 128k
+ * @author cbx
+ * 
+ *
+ */
+public class javaVMStackSOF {
+	
+	private int stackLength = 1;
+	
+	public void stackAdd(){
+		stackLength ++;
+		stackAdd();
+	}
+
+	public static void main( String[] args ) {
+		// TODO Auto-generated method stub
+		javaVMStackSOF oom = new javaVMStackSOF();
+		try{
+			oom.stackAdd();
+		}
+		catch(Exception e){
+			System.out.println("长度:" + oom.stackLength);
+			e.printStackTrace();
+		}
+
+	}
+
+}
